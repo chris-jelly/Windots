fe74affc3aa08d1c07fc594b135b35b2abfbe85b
FuriousJelly <71089665+FuriousJelly@users.noreply.github.com>
2023-07-27 08:01:55 -0400

    Updated to reflect my profile/setup requirements

--- Profile.ps1
+++ Profile.ps1
@@ -1,48 +1,38 @@
-<# 
-                      7#G~                  
-                    7BB7J#P~                
-                 .?BG!   .?#G!              
-                :B@J       .?BB7            
-             ::  :Y#P~        7BB?.         
-           ^Y#?    :J#G~        !GB?.       
-          !&@!       .?#G!        J@B:      
-       ~^  ^Y#5^       .7BB7    .PB?.  ~^   
-    .!GB7    :Y#5^        !GB7.  ^.    Y#5^ 
-    7&&~       !@@G~       .P@#J.       J@B^
-     :J#G~   ~P#J^?#G!   .?#G~~P#Y:  .7BB7  
-       .?BG7P#J.   .7BB7J#P~    ^5#Y?BG!    
-         .?BJ.        7#G~        ^5B!      
-
-    Author: Scott McKendry
-    Description: PowersShell Profile containing aliases and functions to be loaded when a new PowerShell session is started.
+<#
+    Author: Christopher Jelly
+    Decription: Powershell profile containing customizations, aliases, functions, etc.
 #>
 
-# Imports
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Import Terminal-Icons module - This makes ls (Get-ChildItem) display icons for files and folders
-Import-Module Terminal-Icons
 
 
-# Aliases & Custom Envioronment Variables
+#region moduleImports
+Import-Module PSFramework
+Import-Module Logging
+Import-Module posh-git
+#endregion moduleImports
+
+#region Alias
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Set-Alias -Name su -Value Start-AdminSession
 Set-Alias -Name up -Value Update-Profile
 Set-Alias -Name ff -Value Find-File
 Set-Alias -Name grep -Value Find-String
-Set-Alias -Name touch -Value New-File
+Set-Alias -Name touch -Value New-Item
 Set-Alias -Name df -Value Get-Volume
 Set-Alias -Name sed -Value Set-String
 Set-Alias -Name which -Value Show-Command
 Set-Alias -Name ll -Value Get-ChildItem
 Set-Alias -Name la -Value Get-ChildItem
 Set-Alias -Name l -Value Get-ChildItem
+#endregion Alias
 
-# Putting the FUN in Functions 😎
+#region Functions
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 function Find-WindotsRepository {
     <#
     .SYNOPSIS
-        Finds the local Windots repository. 
+        Finds the local Windots repository.
     #>
     [CmdletBinding()]
     param (
@@ -50,10 +40,11 @@ function Find-WindotsRepository {
         [string]$ProfilePath
     )
 
-    Write-Verbose "Resolving the symbolic link for the profile"
+    Write-Verbose 'Resolving the symbolic link for the profile'
     $profileSymbolicLink = Get-ChildItem $ProfilePath | Where-Object FullName -EQ $PROFILE.CurrentUserAllHosts
     return Split-Path $profileSymbolicLink.Target
 }
+
 function Get-LatestProfile {
     <#
     .SYNOPSIS
@@ -61,26 +52,26 @@ function Get-LatestProfile {
         If the profile is out of date, instructions are displayed on how to update it.
     #>
 
-    Write-Verbose "Checking for updates to the profile"
+    Write-Verbose 'Checking for updates to the profile'
     $currentWorkingDirectory = $PWD
     Set-Location $ENV:WindotsLocalRepo
     $gitStatus = git status
 
-    if ($gitStatus -like "*Your branch is up to date with*") {
-        Write-Verbose "Profile is up to date"
+    if ($gitStatus -like '*Your branch is up to date with*') {
+        Write-Verbose 'Profile is up to date'
         Set-Location $currentWorkingDirectory
         return
-    }
-    else {
-        Write-Verbose "Profile is out of date"
-        Write-Host "Your PowerShell profile is out of date with the latest commit. To update it, run Update-Profile." -ForegroundColor Yellow
+    } else {
+        Write-Verbose 'Profile is out of date'
+        Write-Host 'Your PowerShell profile is out of date with the latest commit. To update it, run Update-Profile.' -ForegroundColor Yellow
         Set-Location $currentWorkingDirectory
     }
-} 
+}
+
 function Start-AdminSession {
     <#
     .SYNOPSIS
-        Starts a new PowerShell session with elevated rights. Alias: su 
+        Starts a new PowerShell session with elevated rights. Alias: su
     #>
     Start-Process wt -Verb runAs
 }
@@ -88,15 +79,15 @@ function Start-AdminSession {
 function Update-Profile {
     <#
     .SYNOPSIS
-        Downloads the latest version of the PowerShell profile from Github and updates the PowerShell profile with the latest version. Alternative to completely restarting the action session. 
+        Downloads the latest version of the PowerShell profile from Github and updates the PowerShell profile with the latest version. Alternative to completely restarting the action session.
         Note that functions won't be updated, this requires a full restart. Alias: up
     #>
-    Write-Verbose "Storing current working directory in memory"
+    Write-Verbose 'Storing current working directory in memory'
     $currentWorkingDirectory = $PWD
-    Write-Verbose "Updating local profile from Github repository"
+    Write-Verbose 'Updating local profile from Github repository'
     Set-Location $ENV:WindotsLocalRepo
     git pull | Out-Null
-    Write-Verbose "Reverting to previous working directory"
+    Write-Verbose 'Reverting to previous working directory'
     Set-Location $currentWorkingDirectory
     Write-Verbose "Re-running profile script from $($PROFILE.CurrentUserAllHosts)"
     .$PROFILE.CurrentUserAllHosts
@@ -112,11 +103,11 @@ function Find-File {
         [Parameter(ValueFromPipeline, Mandatory = $true, Position = 0)]
         [string]$SearchTerm
     )
-    
+
     Write-Verbose "Searching for '$SearchTerm' in current directory and subdirectories"
     $result = Get-ChildItem -Recurse -Filter "*$SearchTerm*" -ErrorAction SilentlyContinue
 
-    Write-Verbose "Outputting results to table"
+    Write-Verbose 'Outputting results to table'
     $result | Format-Table -AutoSize
 }
 
@@ -158,21 +149,6 @@ function Find-String {
     Get-ChildItem | Select-String $SearchTerm
 }
 
-function New-File {
-    <#
-    .SYNOPSIS
-        Creates a new file with the specified name and extension. Alias: touch
-    #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, Position = 0)]
-        [string]$Name
-    )
-
-    Write-Verbose "Creating new file '$Name'"
-    New-Item -ItemType File -Name $Name -Path $PWD | Out-Null
-}
-
 function Set-String {
     <#
     .SYNOPSIS
@@ -194,6 +170,11 @@ function Set-String {
     Write-Verbose "Replacing '$Find' with '$Replace' in '$File'"
     (Get-Content $File).replace("$Find", $Replace) | Set-Content $File
 }
+
+function Get-PublicIP {
+    Invoke-WebRequest -Uri 'https://api.ipify.org'
+}
+
 function Show-Command {
     <#
     .SYNOPSIS
@@ -207,62 +188,134 @@ function Show-Command {
     Write-Verbose "Showing definition of '$Name'"
     Get-Command $Name | Select-Object -ExpandProperty Definition
 }
-function Get-OrCreateSecret {
-    <# 
-    .SYNOPSIS
-        Gets secret from local vault or creates it if it doesn't exist. Requires SecretManagement and SecretStore modules and a local vault to be created.
-        Install Modules with:
-            Install-Module Microsoft.PowerShell.SecretManagement, Microsoft.PowerShell.SecretStore
-        Create local vault with:
-            Install-Module Microsoft.PowerShell.SecretManagement, Microsoft.PowerShell.SecretStore
-            Set-SecretStoreConfiguration -Authentication None -Confirm:$False
 
-        https://devblogs.microsoft.com/powershell/secretmanagement-and-secretstore-are-generally-available/
-    
-    .PARAMETER secretName
-        Name of the secret to get or create. It's recommended to use the username or public key / client id as secret name to make it easier to identify the secret later.
-    
-    .EXAMPLE
-        $password = Get-OrCreateSecret -secretName $username
-    
-    .EXAMPLE
-        $clientSecret = Get-OrCreateSecret -secretName $clientId
+function grep {
+    $input | Out-String -Stream | Select-String $args
+}
 
-    .OUTPUTS
-        System.String
-    #>
-
-    [CmdletBinding()]
+function Get-MailDomainInfo {
     param(
-        [Parameter(Mandatory = $true)]
-        [string]$secretName
+        [parameter(Mandatory = $true)][string]$DomainName,
+        [parameter(Mandatory = $false)][string]$DNSserver
     )
 
-    Write-Verbose "Getting secret $secretName"
-    $secretValue = Get-Secret $secretName -AsPlainText -ErrorAction SilentlyContinue
-
-    if (!$secretValue) {
-        $createSecret = Read-Host "No secret found matching $secretName, create one? Y/N"
-
-        if ($createSecret.ToUpper() -eq "Y") {
-            $secretValue = Read-Host -Prompt "Enter secret value for ($secretName)" -AsSecureString
-            Set-Secret -Name $secretName -SecureStringSecret $secretValue
-            $secretValue = Get-Secret $secretName -AsPlainText
-        }
-        else {
-            throw "Secret not found and not created, exiting"
-        }
+    #Use DNS server 1.1.1.1 when parameter DNSserver is not used
+    if (-not ($DNSserver)) {
+        $DNSserver = '1.1.1.1'
     }
-    return $secretValue
+
+    #Retrieve all mail DNS records
+    $autodiscoverA = (Resolve-DnsName -Name "autodiscover.$($domainname)" -Type A -Server $DNSserver -ErrorAction SilentlyContinue).IPAddress
+    $autodiscoverCNAME = (Resolve-DnsName -Name "autodiscover.$($domainname)" -Type CNAME -Server $DNSserver -ErrorAction SilentlyContinue).NameHost
+    $dkim1 = Resolve-DnsName -Name "selector1._domainkey.$($domainname)" -Type CNAME -Server $DNSserver -ErrorAction SilentlyContinue
+    $dkim2 = Resolve-DnsName -Name "selector2._domainkey.$($domainname)" -Type CNAME -Server $DNSserver -ErrorAction SilentlyContinue
+    $domain = Resolve-DnsName -Name $DomainName -Server $DNSserver -ErrorAction SilentlyContinue
+    $dmarc = (Resolve-DnsName -Name "_dmarc.$($DomainName)" -Type TXT -Server $DNSserver -ErrorAction SilentlyContinue | Where-Object Strings -Match 'DMARC').Strings
+    $mx = (Resolve-DnsName -Name $DomainName -Type MX -Server $DNSserver -ErrorAction SilentlyContinue).NameExchange
+    $spf = (Resolve-DnsName -Name $DomainName -Type TXT -Server $DNSserver -ErrorAction SilentlyContinue | Where-Object Strings -Match 'v=spf').Strings
+
+    #Set variables to Not enabled or found if they can't be retrieved
+    #and stop script if domainname is not valid
+    $errorfinding = 'Not enabled'
+    if ($null -eq $domain) {
+        Write-Warning ('{0} not found' -f $DomainName)
+        return
+    }
+
+    if ($null -eq $dkim1 -and $null -eq $dkim2) {
+        $dkim = $errorfinding
+    } else {
+        $dkim = "$($dkim1.Name) , $($dkim2.Name)"
+    }
+
+    if ($null -eq $dmarc) {
+        $dmarc = $errorfinding
+    }
+
+    if ($null -eq $mx) {
+        $mx = $errorfinding
+    }
+
+    if ($null -eq $spf) {
+        $spf = $errorfinding
+    }
+
+    if (($autodiscoverA).count -gt 1) {
+        $autodiscoverA = $errorfinding
+    }
+
+    if ($null -eq $autodiscoverCNAME) {
+        $autodiscoverCNAME = $errorfinding
+    }
+
+    $info = [PSCustomObject]@{
+        'Domain Name'             = $DomainName
+        'Autodiscover IP-Address' = $autodiscoverA
+        'Autodiscover CNAME '     = $autodiscoverCNAME
+        'DKIM Record'             = $dkim
+        'DMARC Record'            = "$($dmarc)"
+        'MX Record(s)'            = $mx -join ', '
+        'SPF Record'              = "$($spf)"
+    }
+
+    return $info
+
 }
+
+Function Set-PathVariable {
+    param (
+        [string]$AddPath,
+        [string]$RemovePath
+    )
+    $regexPaths = @()
+    if ($PSBoundParameters.Keys -contains 'AddPath') {
+        $regexPaths += [regex]::Escape($AddPath)
+    }
+
+    if ($PSBoundParameters.Keys -contains 'RemovePath') {
+        $regexPaths += [regex]::Escape($RemovePath)
+    }
+
+    $arrPath = $env:Path -split ';'
+    foreach ($path in $regexPaths) {
+        $arrPath = $arrPath | Where-Object { $_ -notMatch "^$path\\?" }
+    }
+    $env:Path = ($arrPath + $addPath) -join ';'
+}
+
+#endregion Functions
+
+Set-PSReadLineOption -HistoryNoDuplicates:$true
+
+# Include (don't RUN) the script by prefixing it with `.`
+. C:\git\quick_scripts\Powershell\F7History.ps1
+
+#region AWS
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+Set-DefaultAWSRegion 'us-east-2'
+# Retrieve Bitwarden jelly_desktop CLI token from AWS SSM
+$env:BWS_ACCESS_TOKEN = Get-SSMParameter -Name /app/bitwarden -WithDecryption:$true | Select-Object -ExpandProperty Value
+# Retrieve openAI token
+$env:OpenAIKey = Get-SSMParameter -Name /app/open_ai_beta -WithDecryption:$true | Select-Object -ExpandProperty Value
+
+#endregion AWS
+
+# Add Bitwarden CLI Program to path
+Set-PathVariable -AddPath 'C:\Program Files\bitwarden-cli'
 
 # Custom Environment Variables
 $ENV:IsAdmin = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 $ENV:WindotsLocalRepo = Find-WindotsRepository -ProfilePath $PSScriptRoot
 
-# Prompt Setup
+
+#region PromptSetup
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-Oh-My-Posh init pwsh --config "$env:POSH_THEMES_PATH/material.omp.json" | Invoke-Expression
+
+#Remove python venv environment variable
+$env:VIRTUAL_ENV_DISABLE_PROMPT = 1
+
+oh-my-posh init pwsh --config 'C:\git\quick_scripts\chris_custom_atomicBit.omp.json' | Invoke-Expression
 
 # Check for updates
 Get-LatestProfile
--- Setup.ps1
+++ Setup.ps1
@@ -1,12 +1,13 @@
-# Elevated privileges required for creating symbolic links 
+# Elevated privileges required for creating symbolic links
 #Requires -RunAsAdministrator
+
 
 Set-Location $PSScriptRoot
 
 # Make sure Oh My Posh is installed
 $ompPath = (Get-Command oh-my-posh).Source
 if (!(Test-Path $ompPath)) {
-    winget install -e -h --id=JanDeDobbeleer.oh-my-posh 
+    winget install -e -h --id=JanDeDobbeleer.oh-my-posh
 }
 
 # Create Symbolic link to Profile.ps1 in PowerShell profile directory
@@ -16,37 +17,36 @@ New-Item -ItemType SymbolicLink -Path $PROFILE.CurrentUserAllHosts -Target (Reso
 Install-Module -Name Terminal-Icons -Repository PSGallery
 
 # Get all installed font families
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing')
 $fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families
 
-# Check if CaskaydiaCove NF is installed
-if ($fontFamilies -notcontains "CaskaydiaCove NF") {
+# Check if MesloLGS is installed
+if ($fontFamilies -notcontains 'CaskaydiaCove NF') {
     # Download and install CaskaydiaCove NF
     $webClient = New-Object System.Net.WebClient
-    $webClient.DownloadFile("https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip", ".\CascadiaCode.zip")
+    $webClient.DownloadFile('https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip', '.\CascadiaCode.zip')
 
-    Expand-Archive -Path ".\CascadiaCode.zip" -DestinationPath ".\CascadiaCode" -Force
+    Expand-Archive -Path '.\CascadiaCode.zip' -DestinationPath '.\CascadiaCode' -Force
     $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
-    Get-ChildItem -Path ".\CascadiaCode" -Recurse -Filter "*.ttf" | ForEach-Object {
-        If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {        
+    Get-ChildItem -Path '.\CascadiaCode' -Recurse -Filter '*.ttf' | ForEach-Object {
+        If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {
             # Install font
             $destination.CopyHere($_.FullName, 0x10)
         }
     }
 
-    Remove-Item -Path ".\CascadiaCode" -Recurse -Force
-    Remove-Item -Path ".\CascadiaCode.zip" -Force
+    Remove-Item -Path '.\CascadiaCode' -Recurse -Force
+    Remove-Item -Path '.\CascadiaCode.zip' -Force
 }
 
-# Import Windows Terminal settings
 $terminalSettings = Get-Content -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" | ConvertFrom-Json -Depth 20
 
-# Add font face property to terminalSettings object
-$font = @{
-    "face" = "CaskaydiaCove Nerd Font Mono"
+
+if ($terminalSettings.profiles.defaults.font.face -ne 'CaskaydiaCove Nerd Font Mono') {
+    $font = @{
+        'face' = 'CaskaydiaCove Nerd Font Mono'
+    }
+    Add-Member -InputObject $terminalSettings.profiles.defaults -MemberType NoteProperty -Name 'font' -Value $font -Force
+
+    $terminalSettings | ConvertTo-Json -Depth 20 | Set-Content -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
 }
-
-Add-Member -InputObject $terminalSettings.profiles.defaults -MemberType NoteProperty -Name "font" -Value $font -Force
-
-# Set Windows Terminal settings
-$terminalSettings | ConvertTo-Json -Depth 20 | Set-Content -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
